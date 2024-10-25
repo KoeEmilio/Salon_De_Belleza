1d752e92f2d24bb53b42d2b5f5760d5b269cad2c
Emilio Olvera <jesusolvera355@gmail.com>
2024-10-25 10:34:43 -0600

    Vistas de recepcionista agregadas al direccionamiento

--- resources/views/layouts/recepcionista.blade.php
+++ resources/views/layouts/recepcionista.blade.php
@@ -18,7 +18,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
     <!-- Styles -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Custom Styles -->
     <style>
