27e7f81fc77eebeb7fa60db3387dabe88728caa5
Daniel <garciaazamardaniel@gmail.com>
2024-12-05 13:57:19 -0600

    si

--- database/migrations/2024_10_18_014601_create_payroll_table.php
+++ database/migrations/2024_10_18_014601_create_payroll_table.php
@@ -22,7 +22,7 @@ class CreatePayrollTable extends Migration
             $table->decimal('overtime_hours', 5, 2)->default(0);
             $table->decimal('bonuses', 10, 2)->nullable()->default(0); 
             $table->decimal('tax', 10, 2)->nullable()->default(0); 
-            $table->decimal('net_salary', 10, 2)->default(0);
+            $table->decimal('net_salary', 10, 2)->nullable()->default(0);
             $table->enum('payment_status', ['Pendiente', 'Pagado'])->default('Pendiente');
             $table->timestamps();
         });
